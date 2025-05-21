deb0784f7c3488711cc9b76f57ad347540768df0
DiyaRai04 <diyarai894@gmail.com>
2025-05-21 22:56:18 +0530

    Add files via upload

---
+++ Displayelementwithposition.java
@@ -0,0 +1,27 @@
+package linkedlist1;
+
+import java.util.LinkedList;
+public class Displayelementwithposition {
+
+
+
+
+
+    public static void main(String[] args) {
+      
+        LinkedList<String> colors = new LinkedList<>();
+        
+      
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Red");
+        colors.add("Yellow");
+        colors.add("Black");
+        colors.add("Pink");
+        
+               System.out.println("Elements and their positions in the linked list:");
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println("Position " + i + ": " + colors.get(i));
+        }
+    }
+}
