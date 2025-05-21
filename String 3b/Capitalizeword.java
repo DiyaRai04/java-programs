4afcd361a529cc9b11ca77eab4e87f414cc54c4e
DiyaRai04 <diyarai894@gmail.com>
2025-05-22 00:01:13 +0530

    Update Capitalizeword.java

--- String 3b/Capitalizeword.java
+++ String 3b/Capitalizeword.java
@@ -1,3 +1,7 @@
+// Q6. Write a Java Program for Capitalizing the first letter of each word. using user defined function
+// capitalizeWords()
+
+
 package example3a;
 
 public class Capitalizeword {
