e8e52887e8573655fcf7c817ced8c283b9d79fbb
DiyaRai04 <diyarai894@gmail.com>
2025-05-21 22:42:22 +0530

    Update Sortcolour.java

--- ArrayList/Sortcolour.java
+++ ArrayList/Sortcolour.java
@@ -1,3 +1,6 @@
+//Write a java program for getting different colors through ArrayList interface and sort them
+//using Collections.sort( ArrayListObj)
+
 package Arraylist;
 import java.util.ArrayList;
 import java.util.Collections;
