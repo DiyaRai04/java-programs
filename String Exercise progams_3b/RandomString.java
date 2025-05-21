5c7378082739abbf3db3963eec90367e4afe9f4e
DiyaRai04 <diyarai894@gmail.com>
2025-05-22 00:11:59 +0530

    Update RandomString.java

--- String Exercise progams_3b/RandomString.java
+++ String Exercise progams_3b/RandomString.java
@@ -1,3 +1,6 @@
+// Q9. Write a Java Program for Creating a random string of a specified length using user defined
+// function generateRandomString()
+
 package example3a;
 import java.util.Random;
 public class RandomString {
