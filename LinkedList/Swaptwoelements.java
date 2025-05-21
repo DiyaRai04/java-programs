db88b14a79b019f76305c68ce2322b46a17215a6
DiyaRai04 <diyarai894@gmail.com>
2025-05-21 23:08:33 +0530

    Update Swaptwoelements.java

--- LinkedList/Swaptwoelements.java
+++ LinkedList/Swaptwoelements.java
@@ -1,3 +1,6 @@
+// Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+// Collections.swap(l_list, 0, 2))
+
 package linkedlist1;
 import java.util.Iterator;
 import java.util.LinkedList;
